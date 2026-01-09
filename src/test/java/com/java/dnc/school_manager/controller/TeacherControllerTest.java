bf1bf58ae2946de91d040d6fb470387323dfc731
Gabriel Santos <gfonseca882@gmail.com>
2026-01-09 19:06:13 -0300

    fix(tests): Replaced deprecated MockBean for MockitoBean
    -Replaced MockBean for MockitoBean in StudentControllerTest and TeacherControllerTest

--- src/test/java/com/java/dnc/school_manager/controller/TeacherControllerTest.java
+++ src/test/java/com/java/dnc/school_manager/controller/TeacherControllerTest.java
@@ -12,8 +12,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -33,7 +33,7 @@ class TeacherControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private TeacherService teacherService;
 
     private Teacher teacher;
